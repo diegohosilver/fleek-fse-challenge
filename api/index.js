cd7f73b1b46751dcc55feb53534c2bb50e0bb7b0
le0dime <ctk.591@gmail.com>
2021-04-19 15:16:48 -0300

    Added authentication with jwt. Added database creation with default user using mongoose.

--- api/index.js
+++ api/index.js
@@ -18,43 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // database initialization
 const db = require("./models");
-
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-    initial();
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
-
-function initial() {
-  Role.estimatedDocumentCount((err, count) => {
-    if (!err && count === 0) {
-      new Role({
-        name: "admin"
-      }).save(err => {
-        if (err) {
-          console.log("error", err);
-        }
-
-        console.log("added 'admin' to roles collection");
-      });
-
-      User
-    }
-  });
-}
+db.initialize();
 
 app.get('/', (req, res) => {
     res.send('Up and running!')
 })
 
+// routes setup
+require('./routes/auth.routes')(app);
+require('./routes/keys.routes')(app);
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
 })
--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-var bcrypt = require("bcryptjs");
+let bcrypt = require("bcryptjs");
 
+const dbConfig = require('../config/db.config');
 const User = require("./user.model");
 
 const db = {};
@@ -14,9 +15,15 @@ db.setDefaultData = function() {
     User.estimatedDocumentCount((err, count) => {
         if (!err && count === 0) {
             new User({
-                username: "Key creator",
+                name: "Key Creator",
                 email: "key-creator@fleek-challenge.com",
                 password: bcrypt.hashSync("9r@QS!2u", 8)
+            }).save(err => {
+                if (err) {
+                    console.log("Error when creating user", err)
+                }
+
+                console.log("added document to user collection")
             })
         }
     });
@@ -30,7 +37,7 @@ db.initialize = function() {
     })
     .then(() => {
         console.log("Successfully connect to MongoDB.");
-        initial();
+        db.setDefaultData();
     })
     .catch(err => {
         console.error("Connection error", err);
